d5e261663d0a276cc0a6227527c27c4fe1dd4e59
DadosCoelho <daldocoelho@gmail.com>
2024-07-26 11:49:52 -0300

    2024-07-26

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import style from "./style";
+
 export default function Title(){
     return(
-        <View>
-            <Text>ONEBITHEALTH teste</Text>
+        <View style = {style.boxTitle}>
+            <Text style={style.textTitle}>ONEBITHEALTH</Text>
         </View>
     )
 }
---
+++ src/components/Title/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+    boxTitle: {
+        alignItems:"center",
+        justifyContent:"center",
+        padding:10,
+    },
+    textTitle:{
+        color:"#FF0043",
+        fontSize:24,
+        fontWeight:"bold",
+    },
+});
+
+export default style
